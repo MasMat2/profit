249a284024ba6d6a5cc42aa3884e94862167f729
Max Munoz <amaxmunoz98@gmail.com>
2025-08-22 16:04:29 -0600

    add(category): backend and frontend

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+
+  @Get('api/category')
+  category() {
+    return this.prismaService.category.findMany();
+  }
+
+
+  @Post('api/category/create')
+  create(@Body() createDto: any) {
+    return this.prismaService.category.create({data: createDto});
   }
 }
