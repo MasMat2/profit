e074ea1330ce0dc7aa618226aff006b00425c111
Max Munoz <amaxmunoz98@gmail.com>
2025-10-09 22:53:59 -0600

    add(shared-modal): dynamic modal

---
+++ profit-web/src/app/views/shared/shared-modal/shared-modal.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  selector: 'app-shared-modal',
+  templateUrl: './shared-modal.component.html',
+  styleUrls: ['./shared-modal.component.css']
+})
+export class SharedModalComponent {
+  @Input() user: any;
+  @Input() modalTitle: string = "";
+  @Input() showSaveButton: boolean = true;
+  @Output() close = new EventEmitter<void>();
+  @Output() save = new EventEmitter<any>();
+
+  editedUser: any;
+
+  ngOnInit() {
+    // Clonar el usuario para no modificar el original directamente
+    this.editedUser = this.user ? { ...this.user } : {};
+  }
+
+  onSave() {
+    this.save.emit(this.editedUser);
+  }
+
+  onClose() {
+    this.close.emit();
+  }
+} 
