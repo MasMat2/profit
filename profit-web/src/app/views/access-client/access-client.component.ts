0fbb6c61ba0c85b6ec0cc34a888bf97b6d5120ec
Max Munoz <amaxmunoz98@gmail.com>
2026-02-28 00:44:20 -0600

    update(access-client): add DigitalPersona SDK

--- profit-web/src/app/views/access-client/access-client.component.ts
+++ profit-web/src/app/views/access-client/access-client.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Client } from '../../services/client.service';
@@ -14,24 +14,50 @@ import { PlansService } from '../../services/plans.service';
   templateUrl: './access-client.component.html',
   styleUrls: ['./access-client.component.css']
 })
-export class AccessClientComponent implements OnInit {
+export class AccessClientComponent implements OnInit, OnDestroy {
 
     newClient: Client = {};
     wizardStep = 1;
     accessComplete = false;
     invalidAccess = false;
 
-    private storageKey = 'enrollClientFormState';
-    
-
+    private loadedScripts: HTMLScriptElement[] = [];
+ 
     constructor(
         private clientDataService: ClientService
     ) {
     }
 
+    public async ngOnInit(): Promise<void> {
+        try {
+            await this.loadScript('assets/scripts/es6-shim.js');
+            await this.loadScript('assets/scripts/websdk.client.bundle.min.js');
+            await this.loadScript('assets/scripts/fingerprint.sdk.min.js');
+            // Now scripts are loaded, you can use their APIs
+        } catch (error) {
+            console.error('Script loading failed:', error);
+        }
+    }
+
+
+    loadScript(src: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (document.querySelector(`script[src="${src}"]`)) {
+                resolve();
+                return;
+            }
+            
+            const script = document.createElement('script');
+            script.src = src;
+            script.onload = () => resolve();
+            script.onerror = () => reject(new Error(`Failed to load script: ${src}`));
+            document.body.appendChild(script);
+            this.loadedScripts.push(script);
+        });
+    }
     
-    public ngOnInit(): void {
-        // this.cargarEstadoFormulario();
+    ngOnDestroy() {
+        this.loadedScripts.forEach(script => script.remove());
     }
 
 
