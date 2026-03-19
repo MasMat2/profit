eb5ea95e42dda60019f2ba2d21af6e85988dc151
Max Munoz <amaxmunoz98@gmail.com>
2026-03-18 22:00:39 -0600

    update(backend): add database credentials

--- api/src/database/database.service.ts
+++ api/src/database/database.service.ts
@@ -8,7 +8,7 @@ export class DatabaseService implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
     this.connection = await mysql.createConnection({
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 3306,
+      port: parseInt(process.env.DB_PORT ?? '3306'),
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
