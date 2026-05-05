4cca717e36cad85e13b87fc4e1297ef5820419b2
Max Munoz <amaxmunoz98@gmail.com>
2026-05-04 18:31:08 -0600

    add(shared-modal): component and service

---
+++ profit-web/src/app/views/shared/shared-modal/shared-modal.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ModalService } from '@services/shared/modal.service';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  providers: [ModalService], // Each modal gets its own instance
+  selector: 'app-shared-modal',
+  templateUrl: './shared-modal.component.html',
+  styleUrls: ['./shared-modal.component.scss']
+})
+export class SharedModalComponent {
+  @Input() modalTitle: string = "";
+  @Input() showSaveButton: boolean = true;
+  @Input() size: 'small' | 'medium' | 'large' = 'medium'; // Default size
+
+  @Output() close = new EventEmitter<void>();
+  @Output() save = new EventEmitter<void>();
+
+  constructor(private modalService: ModalService) { }
+
+  ngOnInit() {
+    this.modalService.onClose$.subscribe(() => {
+      this.close.emit();
+    });
+  }
+
+  onSave() {
+    this.save.emit(); // Avisarle al padre
+    this.modalService.triggerSave(); // Avisarle al hijo
+  }
+
+  onClose() {
+    this.close.emit();
+  }
+} 
