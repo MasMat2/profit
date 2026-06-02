bf879162b3b0315a0a292aba0e82adfaa385b925
Max Munoz <amaxmunoz98@gmail.com>
2026-06-02 01:59:50 -0600

    update(frontend): cleanup app routes

--- profit-web/src/app/app.routes.ts
+++ profit-web/src/app/app.routes.ts
@@ -1,16 +1,8 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./views/main-layout/main-layout.routes').then(m => m.MAIN_LAYOUT_ROUTES)
   }
 ];
-
-
-@NgModule({
-    imports: [RouterModule.forRoot(routes, { useHash: true })],
-    exports: [RouterModule]
-  })
-  export class AppRoutingModule {}
   
