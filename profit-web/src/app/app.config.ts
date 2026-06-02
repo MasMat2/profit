2b33bce74dee49f14787472e6b8937f92f3f85bd
Max Munoz <amaxmunoz98@gmail.com>
2026-06-02 01:58:55 -0600

    update(frontend): move menu and token to shared services

--- profit-web/src/app/app.config.ts
+++ profit-web/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { tokenInterceptor } from './services/token.interceptor';
+import { tokenInterceptor } from './services/shared/token.interceptor';
 
 import { routes } from './app.routes';
 
