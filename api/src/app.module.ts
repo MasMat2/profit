c24943a72fcd51706e396ac60f2758dc3c525545
Max Munoz <amaxmunoz98@gmail.com>
2026-06-26 20:05:26 -0600

    update(backend): remove unused files

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AdministracionModule } from './controllers/administracion/administracion.module';
 import { FormasPagoModule } from './controllers/formas-pago/formas-pago.module';
 import { ClasesModule } from './controllers/clases/clases.module';
@@ -13,7 +11,5 @@ import { ClasesModule } from './controllers/clases/clases.module';
     FormasPagoModule,
     ClasesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
